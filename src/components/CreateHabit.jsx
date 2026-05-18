f972dae6bd2333835fc6c87200aa5f3fc16ff060
RajeshwariV-dev <rajeshwarivalarmani.sde@gmail.com>
2026-05-18 23:01:45 +0530

    Gemini Ai key updated

--- src/components/CreateHabit.jsx
+++ src/components/CreateHabit.jsx
@@ -52,7 +52,7 @@ function CreateHabit() {
         import.meta.env.VITE_GEMINI_API_KEY;
 
       const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
+                   `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`,
         {
           method: "POST",
 
